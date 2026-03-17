e15ae3ce0f8bf993bc777e78367421a7ca6441f1
virendar.kmr <virendar.kmr@gmail.com>
2026-03-17 14:46:23 +0530

    started adding ads, added banner ads in homescreen and ads dependency

---
+++ app/src/main/kotlin/com/dailyshayari/ui/components/AdBanner.kt
@@ -0,0 +1,24 @@
+package com.dailyshayari.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
+@Composable
+fun AdBanner(modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier.fillMaxWidth(),
+        factory = { context ->
+            AdView(context).apply {
+                setAdSize(AdSize.BANNER)
+                // Test Banner Ad Unit ID
+                adUnitId = "ca-app-pub-3940256099942544/6300978111"
+                loadAd(AdRequest.Builder().build())
+            }
+        }
+    )
+}
